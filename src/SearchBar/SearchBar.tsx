a42a63964a8ade94b765dc2b6fe65337f69d653b
FinalJest <alicevi@yandex.ru>
2020-06-25 16:44:39 +0500

    Basic request stuff

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, SyntheticEvent, RefObject, useRef, useMemo } from 'react';
+import React, { useState, useCallback, Ref, SyntheticEvent, RefObject, useRef, useMemo } from 'react';
 import Downshift, { DownshiftState, StateChangeOptions } from 'downshift';
 import styled from 'styled-components';
 
@@ -34,75 +34,65 @@ interface SearchSuggestItem {
 export default function SearchBar(): React.ReactElement {
     const [items, setItems] = useState([]);
     const inputElement = useRef<HTMLInputElement>(null);
-
-    const stateReducer = useMemo(() => (state: DownshiftState<SearchSuggestItem>, changes: StateChangeOptions<SearchSuggestItem>) => {
-        switch (changes.type) {
-          case Downshift.stateChangeTypes.changeInput: {
-            const inputData = inputElement.current;
+    const onInputChange = useMemo(() => () => {
+        const inputData = inputElement.current ? inputElement.current.value : null;
 
             if (inputData) {
-                console.log(inputElement.current.value);
-                fetch(`https://suggests.go.mail.ru/sg_ntp?ush=1&get_nvg=1&q=${inputElement.current.value}`)
+                fetch(`https://suggests.go.mail.ru/sg_ntp?ush=1&get_nvg=1&q=${inputData}`)
                     .then((response) => response.json())
-                    .then((data) => console.log(data));
-            }
+                    .then((data) => {
+                        const newItems = data.items.map((item: any) => ({ value: item.text }));
 
-            return changes;
-          }
-          default:
-            return changes
-        }
-      }, []);
+                        console.log(newItems);
+                        setItems(newItems);
+                    });
+            } else {
+                setItems([]);
+            }
+    }, []);
 
     return (
         <Downshift
-            onChange={selection =>
-            alert(selection ? `You selected ${selection.value}` : 'Selection Cleared')
-            }
             itemToString={item => (item ? item.value : '')}
-            stateReducer={stateReducer}
         >
             {({
             getInputProps,
             getItemProps,
-            getLabelProps,
             getMenuProps,
             isOpen,
-            inputValue,
             highlightedIndex,
             selectedItem,
             getRootProps,
             }) => (
-                <div>
-                    <Wrapper>
-                        {/* <label {...getLabelProps()}>Enter a fruit</label> */}
-                        <InputBlock>
-                            <SearchInputWrapper ref={inputElement} {...getInputProps()} />
-                        </InputBlock>
-                        <ul {...getMenuProps()}>
-                        {isOpen
-                            ? items
-                                .filter(item => !inputValue || item.value.includes(inputValue))
-                                .map((item, index) => (
-                                <li
-                                    {...getItemProps({
-                                    key: item.value,
-                                    index,
-                                    item,
-                                    style: {
-                                        backgroundColor:
-                                        highlightedIndex === index ? 'lightgray' : 'white',
-                                        fontWeight: selectedItem === item ? 'bold' : 'normal',
-                                    },
-                                    })}
-                                >
-                                    {item.value}
-                                </li>
-                                ))
-                            : null}
-                        </ul>
-                    </Wrapper>
-            </div>
+                <Wrapper {...getRootProps()}>
+                    <InputBlock>
+                        <SearchInputWrapper {...getInputProps({
+                            onChange: onInputChange,
+                            ref: inputElement
+                        })} />
+                    </InputBlock>
+                    <ul {...getMenuProps()}>
+                    {isOpen
+                        ? items
+                            .map((item, index) => (
+                            <li
+                                {...getItemProps({
+                                key: item.value,
+                                index,
+                                item,
+                                style: {
+                                    backgroundColor:
+                                    highlightedIndex === index ? 'lightgray' : 'white',
+                                    fontWeight: selectedItem === item ? 'bold' : 'normal',
+                                },
+                                })}
+                            >
+                                {item.value}
+                            </li>
+                            ))
+                        : null}
+                    </ul>
+                </Wrapper>
             )}
         </Downshift>
     );
