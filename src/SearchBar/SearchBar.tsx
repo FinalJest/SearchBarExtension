ccc54bf04ed0a553424429a195c271e8eb19864d
Nikitin Andrei <andrei.nikitin@mail.msk>
2020-06-26 10:50:07 +0300

    Base is working

--- src/SearchBar/SearchBar.tsx
+++ src/SearchBar/SearchBar.tsx
@@ -14,36 +14,90 @@ const SearchInput = styled.input`
     flex: 1;
 `;
 
-const SearchInputWrapper = React.forwardRef<HTMLInputElement>((props, ref) => {
-    return <SearchInput ref={ref} {...props} />;
-  });
-  
-
-// const items = [
-//     {value: 'apple'},
-//     {value: 'pear'},
-//     {value: 'orange'},
-//     {value: 'grape'},
-//     {value: 'banana'},
-// ]
-
-interface SearchSuggestItem {
+interface ISearchSuggestItem {
     value: string
 }
 
+interface IRichData {
+    name?: string;
+    source?: string;
+    value?: number | any;
+    day_temp?: string;
+    local_url?: string; // TODO: Типизировать на урл?
+    height?: number; // TODO: Понять зачем нужно
+    width?: number; // TODOL Понять зачем нужно
+}
+
+interface ISuggestApiResponseItem {
+    text: string;
+    type: 'lite' | string; // TODO: Типизировать на enum и понять смысл
+    textMarked: string; // TODO: Нужно понять, зачем это поле
+
+    rich_category?: string; // TODO: Типизировать на enum и понять, зачем нужно
+    rich_data?: IRichData;
+}
+
+interface ISuggestApiResponse {
+    csrf_token: string;
+    items: ISuggestApiResponseItem[];
+    qid: string;
+    sites: any[]; // TODO: Нужно понять, зачем это и что там
+    terms: { query: string }; // TODO: Понять что тут может быть и при необходимости вынести в отдельный интерфейс
+}
+
+// Нужно чтобы работали тайпинги между styled-components и downshift
+type IntrinsicInput = JSX.IntrinsicElements['input'] & { ref?: Ref<HTMLInputElement> };
+
 export default function SearchBar(): React.ReactElement {
     const [items, setItems] = useState([]);
+    const [currentValue, setCurrentValue] = useState<ISearchSuggestItem>(null);
     const inputElement = useRef<HTMLInputElement>(null);
-    const onInputChange = useMemo(() => () => {
+
+    const openSearchTab = useCallback((value: string) => {
+        chrome.tabs.update({ 
+            url: `https://go.mail.ru/search?q=${value}`
+        });
+    }, [currentValue]);
+    const onInputKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            openSearchTab(currentValue.value);
+        }
+    }, [currentValue]);
+
+    const handleStateChange = useCallback((changes: StateChangeOptions<any>) => {
+        switch (changes.type) {
+            case Downshift.stateChangeTypes.changeInput: {
+                setCurrentValue({ value: changes.inputValue });
+
+                if (changes.inputValue !== '') {
+                    fetch(`https://suggests.go.mail.ru/sg_ntp?ush=1&get_nvg=1&q=${changes.inputValue}`)
+                        .then((response) => response.json())
+                        .then((data: ISuggestApiResponse) => {
+                            setItems(data.items.map((item) => ({ value: item.text })));
+                        });
+                } else {
+                    setItems([]);
+                }
+
+                return;
+            }
+            case Downshift.stateChangeTypes.keyDownEnter:
+            case Downshift.stateChangeTypes.clickItem:
+                openSearchTab(changes.selectedItem.value);
+
+                return;
+        }
+    }, []);
+
+    const onInputChange = useCallback(() => {
         const inputData = inputElement.current ? inputElement.current.value : null;
 
             if (inputData) {
                 fetch(`https://suggests.go.mail.ru/sg_ntp?ush=1&get_nvg=1&q=${inputData}`)
                     .then((response) => response.json())
-                    .then((data) => {
-                        const newItems = data.items.map((item: any) => ({ value: item.text }));
+                    .then((data: ISuggestApiResponse) => {
+                        const newItems = data.items.map((item) => ({ value: item.text }));
 
-                        console.log(newItems);
                         setItems(newItems);
                     });
             } else {
@@ -53,7 +107,9 @@ export default function SearchBar(): React.ReactElement {
 
     return (
         <Downshift
+            selectedItem={currentValue}
             itemToString={item => (item ? item.value : '')}
+            onStateChange={handleStateChange}
         >
             {({
             getInputProps,
@@ -66,10 +122,15 @@ export default function SearchBar(): React.ReactElement {
             }) => (
                 <Wrapper {...getRootProps()}>
                     <InputBlock>
-                        <SearchInputWrapper {...getInputProps({
-                            onChange: onInputChange,
-                            ref: inputElement
-                        })} />
+                        <SearchInput 
+                            {...getInputProps({
+                                onChange: onInputChange,
+                                onKeyDown: onInputKeyDown,
+                                ref: inputElement,
+                                isOpen,
+                                placeholder: 'Enter Search Query'
+                            }) as IntrinsicInput}
+                        />
                     </InputBlock>
                     <ul {...getMenuProps()}>
                     {isOpen
--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,8 @@ import React from 'react';
 import SearchBar from './SearchBar/SearchBar';
 import * as types from 'styled-components/cssprop'
 
+declare global {
+    const chrome: any;
+}
+
 ReactDOM.render(<SearchBar />, document.getElementById('root'));
